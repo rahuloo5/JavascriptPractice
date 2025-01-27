9b2c0697577c30f0cadf4b0ac69bed2ca30e8c84
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-01-27 17:12:46 +0530

    frequentElement.js
    Given an integer array nums and an integer k, return the k most frequent elements within the array.
    
    The test cases are generated such that the answer is always unique.
    
    You may return the output in any order.
    
    Example 1:
    
    Input: nums = [1,2,2,3,3,3], k = 2
    
    Output: [2,3]

---
+++ frequentElement.js
@@ -0,0 +1,35 @@
+ topKFrequent(nums, k) {
+        const count = {};
+        for (const num of nums) {
+            count[num] = (count[num] || 0) + 1;
+        }
+
+        const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)]);
+        arr.sort((a, b) => b[0] - a[0]);
+
+        return arr.slice(0, k).map(pair => pair[1]);
+    }
+
+
+-------------------------------------------------------------------
+  topKFrequent(nums, k) {
+        const count = {};
+        const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+        for (const n of nums) {
+            count[n] = (count[n] || 0) + 1;
+        }
+        for (const n in count) {
+            freq[count[n]].push(parseInt(n));
+        }
+
+        const res = [];
+        for (let i = freq.length - 1; i > 0; i--) {
+            for (const n of freq[i]) {
+                res.push(n);
+                if (res.length === k) {
+                    return res;
+                }
+            }
+        }
+    }
