765f12fee301b8ec0847cc4c108a78b1c52921a2
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-01-29 17:04:11 +0530

    Create asyncfunctionsinparallel.js
    Implement a function in JavaScript that takes a list of async functions as input and a callback function and executes the async tasks in parallel that is all at once and invokes the callback after every task is executed.

---
+++ asyncfunctionsinparallel.js
@@ -0,0 +1,50 @@
+function asyncParallel(tasks, callback) {
+  // store the result
+  const results = [];
+  
+  // track the task executed
+  let tasksCompleted = 0;
+  
+  // run each task
+  tasks.forEach(asyncTask => {
+    
+    // invoke the async task
+    // it can be a promise as well
+    // for a promise you can chain it with then
+    asyncTask(value => {
+      // store the output of the task
+      results.push(value);
+      
+      // increment the tracker
+      tasksCompleted++;
+    
+      // if all tasks are executed 
+      // invoke the callback
+      if (tasksCompleted >= tasks.length) {
+        callback(results);
+      }
+    });
+  });
+};
+
+function createAsyncTask() {
+  const value = Math.floor(Math.random() * 10);
+  return function(callback) {
+    setTimeout(() => {
+      callback(value);
+    }, value * 1000);
+  };
+}
+
+const taskList = [
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask()
+];
+
+asyncParallel(taskList, result => {
+  console.log('results', result);
+});
