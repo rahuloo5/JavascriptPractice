a769567abe1a4f5dab72aa6b58c695cdcb97e166
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-05 00:55:59 +0530

    Update arrayShuffle.js

--- arrayShuffle.js
+++ arrayShuffle.js
@@ -5,3 +5,12 @@ function shuffle(arr) {
         [arr[randInd], arr[arr.length - (i + 1)]] = [arr[arr.length - (i + 1)], arr[randInd]]
     }
 }
+
+
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i+1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
