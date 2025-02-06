e0af8fcb80ed266c880e0c4740e41501c65241fd
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-06 16:27:44 +0530

    Create excludesItem.js
    // Given an input of array, 
    // which is made of items with >= 3 properties
    let items = [
      {color: 'red', type: 'tv', age: 18}, 
      {color: 'silver', type: 'phone', age: 20},
      {color: 'blue', type: 'book', age: 17}
    ] 
    // an exclude array made of key value pair
    const excludes = [ 
      {k: 'color', v: 'silver'}, 
      {k: 'type', v: 'tv'}, 
      ...
    ] 
    function excludeItems(items, excludes) { 
      excludes.forEach( pair => { 
        items = items.filter(item => item[pair.k] === item[pair.v])
      })
     
      return items
    }

---
+++ excludesItem.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {object[]} items
+ * @excludes { Array< {k: string, v: any} >} excludes
+ */
+
+/**
+ * @param {object[]} items
+ * @param { Array< {k: string, v: any} >} excludes
+ * @return {object[]}
+ */
+function excludeItems(items, excludes) {
+   return items.filter(item => {
+    return excludes.every(pair => item[pair.k] !== pair.v);
+  })
+}
