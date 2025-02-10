8487b603d80b497ed3734a63ffdf486c2b11b989
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-10 17:54:58 +0530

    Create promiseRacePolyfill.js

---
+++ promiseRacePolyfill.js
@@ -0,0 +1,16 @@
+function race(promises) {
+  let isSettled = false
+  return new Promise((resolve, reject) => {
+    promises.forEach(promise => promise.then((data) => {
+      if (!isSettled) {
+        resolve(data)
+        isSettled = true
+      }
+    }, (error) => {
+      if (!isSettled) {
+        reject(error)
+        isSettled = true
+      }
+    }))
+  })
+}
