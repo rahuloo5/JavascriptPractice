369c0b7517f0068d322496d92817fb6e310416c8
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-12 16:51:27 +0530

    Create MAPPolyfill.js
    Map polyfill

---
+++ MAPPolyfill.js
@@ -0,0 +1,46 @@
+class NodeStore {
+  constructor() {
+    this.keys = [];
+    this.values = [];
+  }
+  set(node, value) {
+    const index = this.keys.indexOf(node);
+    if (index >= 0) {
+      this.values[index] = value;
+    } else {
+      this.keys.push(node);
+      this.values.push(value);
+    }
+  }
+  get(node) {
+    const index = this.keys.indexOf(node);
+    return index >= 0 ? this.values[index] : undefined;
+  }
+  has(node) {
+    return !!this.get(node);
+  }
+}
+
+//o(n)
+----**********************--------------------
+  class NodeStore {
+  constructor() {
+    this.values = [];
+  }
+  set(node, value) {
+    if (node.dataset.nodeStoreId) {
+      this.values[node.dataset.nodeStoreId] = value;
+    } else {
+      node.dataset.nodeStoreId = this.values.length;
+      this.values.push(value);
+    }
+  }
+  get(node) {
+    return this.values[node.dataset.nodeStoreId];
+  }
+  has(node) {
+    return !!node.dataset.nodeStoreId;
+  }
+}
+
+//O(1)
