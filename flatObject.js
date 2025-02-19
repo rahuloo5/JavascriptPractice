c9fc42252daca70ca301627d4f3f9f39f15f53fc
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-19 16:56:31 +0530

    Create flatObject.js
    const nested = {
      A: "12",
      B: 23,
      C: {
        P: 23,
        O: {
           L: 56
        },
        Q: [1, 2]
       }   
    };
    
    console.log(flatten(nested));
    
    Output:
    {
      "A": "12"
      "B": 23,
      "C.O.L": 56,
      "C.P": 23,
      "C.Q.0": 1,
      "C.Q.1": 2,
    }

---
+++ flatObject.js
@@ -0,0 +1,11 @@
+function deepFlattenToObject(obj, prefix = '') {
+  return Object.keys(obj).reduce((acc, k) => {
+    const pre = prefix.length ? prefix + '_' : '';
+    if (typeof obj[k] === 'object' && obj[k] !== null) {
+      Object.assign(acc, deepFlattenToObject(obj[k], pre + k));
+    } else {
+      acc[pre + k] = obj[k];
+    }
+    return acc;
+  }, {});
+}
