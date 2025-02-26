51a38dd2fcffb16c76a7ff0ba24c5c7dc9aa78dc
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-26 12:32:15 +0530

    Create functionexpression.js

---
+++ functionexpression.js
@@ -0,0 +1,25 @@
+function a(){
+}
+const b = function() {
+  
+}
+
+const c = function d() {
+  console.log(typeof d)
+  d = 'e'
+ 
+  console.log(typeof d)
+}
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+c()
+
+// function
+// function
+// function
+// undefined
+// function
+// function
