b5dcba8ea9ad31816aeb98a02cb376f6eca44fad
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-02-27 13:29:08 +0530

    Update auto-retry.js

--- auto-retry.js
+++ auto-retry.js
@@ -1,17 +1,19 @@
 function fetchWithAutoRetry(fetcher, maximumRetryCount) {
-  return new Promise((resolve, reject) => {
-    let retryCount = 0
-    const callFetcher = () => fetcher().then((data) => {
-      resolve(data)
-    }, (error) => {
-      if (retryCount < maximumRetryCount) {
-        callFetcher()
-        retryCount += 1
-      } else {
-        reject(error)
-      }
+  // your code here
+   return new Promise((resolve,reject)=>{
+        let retry =0
+        
+        const getData =()=>fetcher().then((data)=>{
+            resolve(data)
+        }).catch((error)=>{
+            if(retry<maximumRetryCount){
+                getData();
+                retry = retry+1
+            } else{
+                reject(error)
+            }
+        })
+       getData() 
+        
     })
-    
-    callFetcher()
-  })
 }
