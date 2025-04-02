fd7bf687cd3c742b3155e3012386301a67abcbe5
Rahul Singh Tomar <45355156+rahuloo5@users.noreply.github.com>
2025-04-02 12:52:55 +0530

    Create vowelSubstring.js
    You are given an integer array nums consisting of n elements, and an integer k.
    
    Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
    
     
    
    Example 1:
    
    Input: nums = [1,12,-5,-6,50,3], k = 4
    Output: 12.75000
    Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
    Example 2:
    
    Input: nums = [5], k = 1
    Output: 5.00000

---
+++ vowelSubstring.js
@@ -0,0 +1,28 @@
+function check(s){
+    if(s=="a" ||s=="e"||s=="i"||s=="o"||s=="u"){
+        return true
+    }
+    return false
+}
+var maxVowels = function(s, k) {
+    let max =0, count=0; 
+    for(let i=0;i<k;i++){
+        if(check(s[i])){
+            count++
+        }
+
+    }
+    max = count
+    for(let i=k; i<s.length; i++){
+        if(check(s[i])){
+            count++
+        }
+        if(check(s[i-k])){
+            count--
+        }
+        max = Math.max(max, count)
+    }
+    return max
+
+    
+};
